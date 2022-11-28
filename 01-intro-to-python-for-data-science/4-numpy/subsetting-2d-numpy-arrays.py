8ee7ded324c49c74d6fdc25a0ad2f3364e4eae99
KAMEEL5 <91517880+KAMEEL5@users.noreply.github.com>
2022-11-28 14:50:42 +0300

    Update subsetting-2d-numpy-arrays.py
    update np_weight_lb variable

--- 01-intro-to-python-for-data-science/4-numpy/subsetting-2d-numpy-arrays.py
+++ 01-intro-to-python-for-data-science/4-numpy/subsetting-2d-numpy-arrays.py
@@ -41,8 +41,8 @@ np_baseball = np.array(baseball)
 # Print out the 50th row of np_baseball
 print(np_baseball[49,:])
 
-# Select the entire second column of np_baseball: np_weight
-np_weight = np_baseball[:,1]
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:,1]
 
 # Print out height of 124th player
 print(np_baseball[123,0])
